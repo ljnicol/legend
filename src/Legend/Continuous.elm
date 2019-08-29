385464a0e415c33572ca683cfb2eefca5422d1ce
ljnicol <l.nicol.9@gmail.com>
2019-08-29 19:22:29 +1000

    Move global config variables into record

--- src/Legend/Continuous.elm
+++ src/Legend/Continuous.elm
@@ -1,4 +1,4 @@
-module Legend.Continuous exposing (..)
+module Legend.Continuous exposing (view)
 
 import Chroma.Chroma as Chroma
 import Chroma.Converter.Out.ToHex as ChromaToHex
@@ -11,53 +11,40 @@ import Svg
 import Svg.Attributes as SvgAttributes
 
 
-numberOfStops : Int
-numberOfStops =
-    102
+type alias Config =
+    { numberOfStops : Int
+    , segmentWidth : Int
+    , padding : Int
+    , svgHeight : Float
+    , unitsYTranslate : Float
+    , textTopPadding : Int
+    , segmentHeight : Int
+    , colorYTranslate : Float
+    , tickWidth : Int
+    }
 
 
-segmentWidth : Int
-segmentWidth =
-    2
-
-
-padding : Int
-padding =
-    26
-
-
-svgHeight : Float
-svgHeight =
-    46.0
-
-
-unitsYTranslate : Float
-unitsYTranslate =
-    34.0
-
-
-textTopPadding : Int
-textTopPadding =
-    14
-
-
-segmentHeight : Int
-segmentHeight =
-    24
-
-
-colorYTranslate : Float
-colorYTranslate =
-    4.0
-
-
-tickWidth : Int
-tickWidth =
-    1
+defaultConfig : Config
+defaultConfig =
+    { numberOfStops = 102
+    , segmentWidth = 2
+    , padding = 26
+    , svgHeight = 46.0
+    , unitsYTranslate = 34.0
+    , textTopPadding = 14
+    , segmentHeight = 24
+    , colorYTranslate = 4.0
+    , tickWidth = 1
+    }
 
 
 view : List (Legend.Bin a) -> (a -> String) -> Html.Html msg
 view bins show =
+    viewWithConfig bins show defaultConfig
+
+
+viewWithConfig : List (Legend.Bin a) -> (a -> String) -> Config -> Html.Html msg
+viewWithConfig bins show ({ numberOfStops, segmentWidth, padding, svgHeight } as config) =
     let
         -- 4 pixels wide * 102 -> 408
         totalSvgWidth =
@@ -71,7 +58,7 @@ view bins show =
                     , SvgAttributes.height <| String.fromFloat svgHeight
                     , SvgAttributes.fill "white"
                     ]
-                    [ viewBody nonEmptyBins show
+                    [ viewBody nonEmptyBins show config
                     ]
                 ]
 
@@ -81,8 +68,8 @@ view bins show =
                 ]
 
 
-viewBody : Nonempty.Nonempty (Legend.Bin a) -> (a -> String) -> Svg.Svg msg
-viewBody bins show =
+viewBody : Nonempty.Nonempty (Legend.Bin a) -> (a -> String) -> Config -> Svg.Svg msg
+viewBody bins show ({ colorYTranslate } as config) =
     let
         colours =
             Nonempty.map (\b -> ChromaTypes.RGBAColor b.color) bins
@@ -92,23 +79,23 @@ viewBody bins show =
     in
     Svg.g
         [ SvgAttributes.transform <| "translate(0, " ++ String.fromFloat colorYTranslate ++ ")" ]
-        (viewColourBand colours
-            ++ viewTicks values
+        (viewColourBand colours config
+            ++ viewTicks values config
         )
 
 
-viewColourBand : Nonempty.Nonempty ChromaTypes.ExtColor -> List (Svg.Svg msg)
-viewColourBand colours =
+viewColourBand : Nonempty.Nonempty ChromaTypes.ExtColor -> Config -> List (Svg.Svg msg)
+viewColourBand colours ({ numberOfStops } as config) =
     let
         ( _, f ) =
             Chroma.domain (Nonempty.Nonempty 0 [ toFloat numberOfStops ]) colours
     in
-    viewStopColor f 0
-        :: List.map (\i -> viewStopColor f i) (List.range 1 numberOfStops)
+    viewStopColor f 0 config
+        :: List.map (\i -> viewStopColor f i config) (List.range 1 numberOfStops)
 
 
-viewTicks : Nonempty.Nonempty String -> List (Svg.Svg msg)
-viewTicks values =
+viewTicks : Nonempty.Nonempty String -> Config -> List (Svg.Svg msg)
+viewTicks values ({ numberOfStops } as config) =
     let
         numberOfTicks =
             List.length ticks
@@ -125,12 +112,12 @@ viewTicks values =
         max =
             reverseTicks |> Nonempty.head
     in
-    List.indexedMap (\index tick -> viewTick (tickPosition index) tick) ticks
-        ++ [ viewLabel numberOfStops max ]
+    List.indexedMap (\index tick -> viewTick (tickPosition index) tick config) ticks
+        ++ [ viewLabel numberOfStops max config ]
 
 
-viewTick : Float -> String -> Svg.Svg msg
-viewTick position label =
+viewTick : Float -> String -> Config -> Svg.Svg msg
+viewTick position label ({ segmentWidth, segmentHeight, tickWidth, padding } as config) =
     Svg.g []
         [ Svg.rect
             [ (SvgAttributes.x << String.fromFloat) <|
@@ -141,12 +128,12 @@ viewTick position label =
             , SvgAttributes.fill "white"
             ]
             []
-        , viewLabel (floor position) label
+        , viewLabel (floor position) label config
         ]
 
 
-viewStopColor : (Float -> ChromaTypes.ExtColor) -> Int -> Svg.Svg msg
-viewStopColor f index =
+viewStopColor : (Float -> ChromaTypes.ExtColor) -> Int -> Config -> Svg.Svg msg
+viewStopColor f index { segmentWidth, segmentHeight, tickWidth, padding } =
     Svg.rect
         [ SvgAttributes.x << String.fromInt <| ((index + 1) * segmentWidth + padding)
         , SvgAttributes.y "0"
@@ -157,8 +144,8 @@ viewStopColor f index =
         []
 
 
-viewLabel : Int -> String -> Svg.Svg msg
-viewLabel index label =
+viewLabel : Int -> String -> Config -> Svg.Svg msg
+viewLabel index label { segmentWidth, segmentHeight, tickWidth, padding, textTopPadding } =
     Svg.text_
         [ SvgAttributes.x << String.fromInt <| (index + 1) * segmentWidth + padding
         , SvgAttributes.y << String.fromInt <| segmentHeight + textTopPadding
